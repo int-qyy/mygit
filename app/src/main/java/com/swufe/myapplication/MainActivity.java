7a68d7c3e0f1c01830f0e08ccea50171d3922910
int-qyy <1436268171@qq.com>
2020-09-21 20:56:41 +0800

    代码提交到GitHub

--- app/src/main/java/com/swufe/myapplication/MainActivity.java
+++ app/src/main/java/com/swufe/myapplication/MainActivity.java
@@ -8,7 +8,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView out;
+    TextView out1;
+    TextView out2;
     EditText edit;
 /**
     @Override
@@ -26,28 +27,44 @@ public class MainActivity extends AppCompatActivity {
     }
     **/
     int i=0;
+    int j=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation);
-        out=(TextView)findViewById(R.id.img);
+        out1=(TextView)findViewById(R.id.out1);
+        out2=(TextView)findViewById(R.id.out2);
     }
     public void btn1(View view){
-        i=i+1;
-        out.setText("得分为"+i);
+        i=i+3;
+        out1.setText(i);
     }
     public void btn2(View view){
         i=i+2;
-        out.setText("得分为"+i);
+        out1.setText(i);
     }
     public void btn3(View view){
-        i=i+3;
-        out.setText("得分为"+i);
+        i=i+1;
+        out1.setText(i);
+    }
+
+    public void btn5(View view){
+        j=j+3;
+        out2.setText(j);
+    }
+    public void btn6(View view){
+        j=j+2;
+        out2.setText(j);
+    }
+    public void btn7(View view){
+        j=j+1;
+        out2.setText(j);
     }
     public void btn4(View view){
         i=0;
-        out.setText("得分为"+i);
+        j=0;
+        out1.setText(i);
+        out2.setText(j);
     }
-
 }
--- app/src/main/java/com/swufe/myapplication/calculation.java
+++ app/src/main/java/com/swufe/myapplication/calculation.java
@@ -7,30 +7,48 @@ import android.view.View;
 import android.widget.TextView;
 
 public class calculation extends AppCompatActivity {
-    TextView out;
+    TextView out1;
+    TextView out2;
+
     int i=0;
+    int j=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation);
-        out=(TextView)findViewById(R.id.img);
+        out1=(TextView)findViewById(R.id.out1);
+        out2=(TextView)findViewById(R.id.out2);
     }
     public void btn1(View view){
-        i=i+1;
-        out.setText("Team A得分为"+i);
+        i=i+3;
+        out1.setText(i);
     }
     public void btn2(View view){
         i=i+2;
-        out.setText("Team A得分为"+i);
+        out1.setText(i);
     }
     public void btn3(View view){
-        i=i+3;
-        out.setText("Team A得分为"+i);
+        i=i+1;
+        out1.setText(i);
     }
 
-    public void btn4(View view) {
+    public void btn5(View view){
+        j=j+3;
+        out2.setText(i);
+    }
+    public void btn6(View view){
+        j=j+2;
+        out2.setText(j);
+    }
+    public void btn7(View view){
+        j=j+1;
+        out2.setText(j);
+    }
+    public void btn4(View view){
         i=0;
-        out.setText("Team A得分为"+i);
+        j=0;
+        out1.setText(i);
+        out2.setText(j);
     }
 }
